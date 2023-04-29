28e7b786027d5c7c71de380de5736a265a2b12ac
ichinose9372 <107186124+ichinose9372@users.noreply.github.com>
2023-04-29 23:36:35 +0900

    home moniter set

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yichinos <yichinos@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: ichinoseyuuki <ichinoseyuuki@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 12:51:07 by yichinos          #+#    #+#             */
-/*   Updated: 2023/04/29 18:33:52 by yichinos         ###   ########.fr       */
+/*   Updated: 2023/04/29 23:24:43 by ichinoseyuu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,18 +42,12 @@ t_data	**check_args_and_malloc(int argc, char **argv, t_data **data)
 	return (data);
 }
 
-void	init_data(char **argv, t_data **data)
+void	init_data(char **argv, t_data **data, t_moniter *monu)
 {
 	int	i;
 	int	num;
 
 	num = atoi(argv[1]);
-	i = 0;
-	while (i < num)
-	{
-		gettimeofday(&(data[i]->time), NULL);
-		i++;
-	}
 	i = 0;
 	while (i < num)
 	{
@@ -63,6 +57,7 @@ void	init_data(char **argv, t_data **data)
 	i = 0;
 	while (i < num)
 	{
+		data[i]->moniter = monu;
 		if (i == num - 1)
 		{
 			data[i]->left_fork = &(data[i]->fork);
@@ -77,6 +72,19 @@ void	init_data(char **argv, t_data **data)
 	}
 }
 
+void	set_moniter(t_moniter	*moniter, t_data	**main, char	**argv)
+{
+
+	moniter->data = main;
+	moniter->t_eat = atoi(argv[2]);
+	moniter->t_sleep = atoi(argv[3]);
+	moniter->t_die = atoi(argv[4]);
+	if (argv[5])
+		moniter->must_eat = atoi(argv[5]);
+	gettimeofday(&(moniter->moniter_time), NULL);
+	pthread_mutex_init(&(moniter->mutex), NULL);
+}
+
 long	cal_time(struct timeval time, struct timeval now)
 {
 	long	now_time;
@@ -85,6 +93,16 @@ long	cal_time(struct timeval time, struct timeval now)
 	now_time = (now.tv_sec) * 1000 + (now.tv_usec) / 1000;
 	st_time = (time.tv_sec) * 1000 + (time.tv_usec) / 1000;
 	return ((time.tv_sec * 1000) + (now_time - st_time));
+}
+
+void	moniter_func(void *arg)
+{
+	t_moniter	*moniter;
+
+	moniter = (t_moniter *)arg;
+	while(1)
+	{
+	}
 }
 
 void	*philo_func(void *arg)
@@ -98,8 +116,6 @@ void	*philo_func(void *arg)
 	printf(" %ld %d is thinking\n", cal_time(data->time, now), data->num_philo);
 	while (1)
 	{
-		if (data->must_eat == 0)
-			break ;
 		if (data->num_philo % 2 == 0)
 		{
 			pthread_mutex_lock(data->left_fork);
@@ -107,11 +123,9 @@ void	*philo_func(void *arg)
 			gettimeofday(&now, NULL);
 			printf(" %ld %d has taken a fork\n", cal_time(data->time, now), data->num_philo);
 			printf(" %ld %d is eating\n", cal_time(data->time, now), data->num_philo);
-			gettimeofday(&(data)->last_eat, NULL);
 			//usleep() eating
 			pthread_mutex_unlock(data->right_fork);
 			pthread_mutex_unlock(data->left_fork);
-			data->must_eat--;
 			printf(" %ld %d thinking\n", cal_time(data->time, now), data->num_philo);
 		}
 		else
@@ -123,9 +137,6 @@ void	*philo_func(void *arg)
 			printf(" %ld %d eating\n", cal_time(data->time, now), data->num_philo);
 			pthread_mutex_unlock(data->left_fork);
 			pthread_mutex_unlock(data->right_fork);
-			gettimeofday(&(data)->last_eat, NULL);
-			usleep(200);
-			data->must_eat--;
 			gettimeofday(&now, NULL);
 			printf(" %ld %d thinking\n", cal_time(data->time, now), data->num_philo);
 		}
@@ -136,13 +147,23 @@ void	*philo_func(void *arg)
 int	main(int argc, char	**argv)
 {
 	t_data			**data;
+	t_moniter		*moniter;
 	int				i;
 
 	i = 0;
 	data = check_args_and_malloc(argc, argv, data);
 	if (data == NULL)
 		return (0);
-	init_data(argv, data);
+	moniter = malloc(sizeof(t_moniter));
+	if (moniter == NULL)
+	{
+		free(data);
+		return (0);
+	}
+	init_data(argv, data, moniter);
+	set_moniter(moniter, data, argv);
+	return (0);
+	pthread_create(&(moniter->pid), NULL, moniter_func, moniter);
 	while (i < atoi(argv[1]))
 	{
 		pthread_create((&(data[i])->pid), NULL, philo_func, data[i]);
--- philo.h
+++ philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yichinos <yichinos@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: ichinoseyuuki <ichinoseyuuki@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 12:45:42 by yichinos          #+#    #+#             */
-/*   Updated: 2023/04/29 18:32:43 by yichinos         ###   ########.fr       */
+/*   Updated: 2023/04/29 23:24:29 by ichinoseyuu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,25 @@
 
 typedef struct s_data
 {
-	int				num_philo;
-	pthread_t		pid;
-	pthread_mutex_t	fork;
-	pthread_mutex_t	*left_fork;
-	pthread_mutex_t	*right_fork;
-	struct timeval	time;
+	int					num_philo;
+	pthread_t			pid;
+	pthread_mutex_t		fork;
+	pthread_mutex_t		*left_fork;
+	pthread_mutex_t		*right_fork;
+	struct timeval		time;
+	struct s_moniter	*moniter;
 }	t_data;
 
 typedef struct s_moniter
 {
-	t_data	**data;
-	int		t_die;
-	int		t_sleep;
-	int		t_eat;
-	int		must_eat;
+	t_data			**data;
+	int				t_die;
+	int				t_sleep;
+	int				t_eat;
+	int				must_eat;
+	pthread_t		pid;
+	struct timeval	moniter_time;
+	pthread_mutex_t	mutex;
 }	t_moniter;
 
 //check_arg
